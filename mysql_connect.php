b217fdc9f9a5e83d605f8a270fa14adc867712eb
petrusgf <petrusgf@gmail.com>
2018-02-11 05:17:57 -0800

    Fixing mysql connect

--- mysql_connect.php
+++ mysql_connect.php
@@ -9,7 +9,3 @@ if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') '
             . $mysqli->connect_error);
 }
-
-echo 'Success... ' . $mysqli->host_info . "\n";
-
-$mysqli->close();
