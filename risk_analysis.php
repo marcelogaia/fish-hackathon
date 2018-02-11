a59873ec6f69ae8cb672645f4f510946b37a1eab
petrusgf <petrusgf@gmail.com>
2018-02-11 04:27:24 -0800

    Risk Analysis test

--- risk_analysis.php
+++ risk_analysis.php
@@ -1,3 +1,10 @@
-<?php 
+<?php require_once "mysql_connect.php";
 
-require_once "mysql_connect.php";
+$country = $_GET['country'];
+$species = $_GET['species'];
+$company = $_GET['company'];
+
+
+$result = $mysqli->query("CALL data_country({$country});");
+
+var_dump($result->fetch_assoc());
