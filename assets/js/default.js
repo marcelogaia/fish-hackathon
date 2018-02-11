b06174212924d85af0f0d43e4ad8e00e72d5a37e
petrusgf <petrusgf@gmail.com>
2018-02-11 05:32:58 -0800

    Adjustments (just too tired to commit)

--- assets/js/default.js
+++ assets/js/default.js
@@ -35,7 +35,7 @@
 
 	$(country).autocomplete({
 	    serviceUrl: 'risk_analysis.php',
-	    lookup: { "country": country.value },
+	    lookup: { "query": country.value, "type" : "country" },
 	    onSelect: function (suggestion) {
 	        country.value = suggestion.name;
 	    }
@@ -44,7 +44,7 @@
 
 	$(species).autocomplete({
 	    serviceUrl: 'risk_analysis.php',
-	    lookup: { "species": species.value },
+	    lookup: { "query": species.value, "type" : "species" },
 	    onSelect: function (suggestion) {
 	        species.value = suggestion.name;
 	    }
@@ -53,7 +53,7 @@
 
 	$(company).autocomplete({
 	    serviceUrl: 'risk_analysis.php',
-	    lookup: { "company": company.value },
+	    lookup: { "query": company.value, "type" : "company" },
 	    onSelect: function (suggestion) {
 	        company.value = suggestion.name;
 	    }
--- risk_analysis.php
+++ risk_analysis.php
@@ -1,15 +1,14 @@
 <?php require_once "mysql_connect.php";
 
-$country = $_GET['country'];
-$species = $_GET['species'];
-$company = $_GET['company'];
+$type = $_GET["type"];
+$query = $_GET['query'];
 
 
-if(isset($_GET['country']))
+if($type == 'country' and $query != "")
 	$result = $mysqli->query("CALL data_countries('{$country}');");
-if(isset($_GET['species']))
+if($type == 'species' and $query != "")
 	$result = $mysqli->query("CALL data_species('{$species}');");
-if(isset($_GET['company']))
+if($type == 'company' and $query != "")
 	$result = $mysqli->query("CALL data_company('{$company}');");
 
 echo json_encode(($result->fetch_assoc()));
