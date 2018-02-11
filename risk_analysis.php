594bb91e2767fc39acd729ba62a7b75da08451a3
petrusgf <petrusgf@gmail.com>
2018-02-11 06:01:18 -0800

    .

--- risk_analysis.php
+++ risk_analysis.php
@@ -1,9 +1,11 @@
 <?php require_once "mysql_connect.php";
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $country = $_GET['country'];
 
-
-if($country == '')
-	$result = $mysqli->query("CALL jorge('{$country}');");
+if($country == '') $result = $mysqli->query("CALL jorge('{$country}')");
 
 echo json_encode(($result->fetch_assoc()));
